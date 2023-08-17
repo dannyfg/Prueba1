2205cd7de625712185b3239449f6c5ea8a5ff745
dannyfg <48659451+dannyfg@users.noreply.github.com>
2023-08-16 18:40:17 -0600

    Fav Song

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("The Way I Loved You (Taylor's Version)-Taylor Swift");
     }
 }
